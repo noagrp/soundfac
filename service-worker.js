5461fbc92ce6897902d949dd9a27b1ee857365b9
noagrp <lihwlee11@gmail.com>
2026-09-18 01:53:09 +0800

    Refresh SoundFac cache strategy

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='soundfac-v3';
+const CACHE='soundfac-v4';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./favicon-32x32.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return res;}).catch(()=>caches.match('./index.html'))));});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const req=event.request;const isPage=req.mode==='navigate'||new URL(req.url).pathname.endsWith('/index.html');if(isPage){event.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return res;}).catch(()=>caches.match('./index.html')));return;}event.respondWith(caches.match(req).then(hit=>hit||fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(cache=>cache.put(req,copy));return res;})));});
